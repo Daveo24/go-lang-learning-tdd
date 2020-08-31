99897fa34f0722a2e39921b06db75583fd4c3a40
Dave Rowntree <drowntree@dxc.com>
2020-08-31 21:33:53 +0100

    extract function

--- hello/hello.go
+++ hello/hello.go
@@ -4,16 +4,34 @@ import (
 	"fmt"
 )
 
+const spanish = "Spanish"
+const french = "French"
+const spanishHelloPrefix = "Hola, "
 const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
 
 // Hello function returns string
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Dave"))
+	fmt.Println(Hello("Dave", "Spanish"))
 }

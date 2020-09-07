6e12fafed97f91a2b119c9c6d09c2ef5b6bd8ab2
Dave Rowntree <drowntree@dxc.com>
2020-09-07 22:20:12 +0100

    arrays and slices

---
+++ arraysandslices/arrays_and_slices.go
@@ -0,0 +1,20 @@
+package arraysandslices
+
+func Sum(numbers []int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, number := range numbersToSum {
+		sums = append(sums, Sum(number))
+	}
+	return sums
+}
